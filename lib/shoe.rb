40da65fa973cf7d80d7546396086d428e87bd559
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-13 14:08:43 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,21 +13,3 @@ class Shoe
   
   
 end
-  
-
-# class Book
-  
-#   attr_reader :title
-#   attr_accessor :author, :genre, :page_count
-  
-#   def initialize(title)
-#     @title = title
-#   end
-  
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
-  
-  
-  
-# end
