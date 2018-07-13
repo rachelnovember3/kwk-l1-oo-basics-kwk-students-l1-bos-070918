2cf6c5597e094b8d8c6f7414763fdc58cd1acee6
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-13 13:59:45 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,3 +2,22 @@
 
 class Shoe
   
+  
+  
+  
+  
+  
+  
+#   class Book
+  
+#   attr_reader :title
+#   attr_accessor :author, :genre, :page_count
+  
+#   def initialize(title,author,genre,page_count)
+#     @title = title
+#     @author = author
+#     @genre = genre
+#     @page_count = page_count
+    
+#   end
+# end
