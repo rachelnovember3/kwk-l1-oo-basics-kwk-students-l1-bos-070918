919f14638c755c8d4ff5094047b2a089d0e0d4dd
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-13 14:19:00 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
 # book.rb
 # Add you Book class here
+class Book
+  
+  attr_reader :title
+  attr_accessor :author, :genre, :page_count
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+  
+  
+end
+
+
+  
+# great_gatsby = Book.new("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 180)
+# puts great_gatsby.title
+# # great_gatsby.author = "F. Scott Fitzgerald"
+# puts great_gatsby.author
+# puts great_gatsby.genre
+# puts great_gatsby.page_count
